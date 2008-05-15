1694ece23dc401cc11be119da0c9f13c4e0da033
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2008-05-15 22:43:10 +0000

    Initial bits to support variable replacement in SVGs

--- trunk/dgs4j/src/com/rtsz/dgs4j/ProcessingEngine/CommandEnginePlugins/Batik/Instructions/substituteVariables.java
+++ trunk/dgs4j/src/com/rtsz/dgs4j/ProcessingEngine/CommandEnginePlugins/Batik/Instructions/substituteVariables.java
@@ -6,6 +6,7 @@
 package ImageProcessor.ProcessingEngine.CommandEnginePlugins.Batik.Instructions;
 
 import ImageProcessor.*;
+import ImageProcessor.ProcessingEngine.*;
 
 import java.util.*;
 
@@ -13,6 +14,12 @@ import org.apache.xerces.parsers.*;
 import org.xml.sax.*;
 import org.w3c.dom.*;
 
+//import java.io.File;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException; 
 /**
  *
  * @author dwimsey
@@ -20,6 +27,64 @@ import org.w3c.dom.*;
 public class substituteVariables implements ImageProcessor.ProcessingEngine.Instructions.IInstruction {
     public boolean process(ImageProcessor.ProcessingEngine.ProcessingWorkspace workspace, Node instructionNode)
     {
+        NamedNodeMap attributes = instructionNode.getAttributes();
+        Node bufferNode = attributes.getNamedItem("buffer");
+        String bufferName = null;
+
+        if(bufferNode == null) {
+            if(!workspace.requestInfo.continueOnError) {
+                workspace.log("Processing halted because the command does not have a buffer attribute: " + instructionNode.getNodeName());
+                return(false);  // this should throw an exception instead
+            } else {
+                workspace.log("Processing of command skipped because it does not have a buffer attributes: " + instructionNode.getNodeName());
+                return(false);  // this should throw an exception instead
+            }
+        } else {
+            bufferName = bufferNode.getNodeValue();
+            if(bufferName == null || bufferName.length()==0) {
+               if(!workspace.requestInfo.continueOnError) {
+                    workspace.log("Processing halted because the command does not have a value for the buffer attribute: " + instructionNode.getNodeName());
+                    return(false);  // this should throw an exception instead
+                } else {
+                    workspace.log("Processing of command skipped because it does not have a value for the buffer attributes: " + instructionNode.getNodeName());
+                    return(false);  // this should throw an exception instead
+                }
+            }
+        }
+
+        ProcessingEngineImageBuffer iBuffer = workspace.getImageBuffer(bufferName);
+        if(!iBuffer.mimeType.equals("image/svg+xml")) {
+            return(false);
+        }
+
+        Document doc = null;
+        try {
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            doc = docBuilder.parse(new java.io.ByteArrayInputStream((byte[])iBuffer.data));
+        } catch (Exception ex) {
+            workspace.log("Unexpected parser error: " + ex.getMessage());
+        }
+
+        NodeList t = doc.getElementsByTagName("text");
+        int i = t.getLength();
+        substituteVars(workspace, doc.getParentNode());
         return(true);
+    }
+    
+    private void substituteVars(ProcessingWorkspace workspace, Node domNode)
+    {
+        
+        if(domNode == null) {
+            return;
+        }
+        String nodeName = domNode.getNodeName();
+        NodeList childNodes = domNode.getChildNodes();
+        if(childNodes != null) {
+            for(int i = 0; i<childNodes.getLength(); i++) {
+                substituteVars(workspace, childNodes.item(i));
+            }
+        }
+        
     }
 }
