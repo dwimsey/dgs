0d192904d3ef7df5a2c9b4b3747a1cd5684d01f0
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2008-05-16 23:05:01 +0000

    Added options class

---
+++ trunk/DGSPreviewer/src/dgspreviewer/Options.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dgspreviewer;
+
+/**
+ *
+ * @author dwimsey
+ */
+public class Options {
+    public java.awt.Color backgroundColor;
+    public Options()
+    {
+        super();
+        backgroundColor = new java.awt.Color(0xFFFFFFFF);
+    }
+}
