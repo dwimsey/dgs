36bf69081b96255ff77bdf21f9ed9e66a2a28b9d
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2008-08-20 04:40:27 +0000

    Updated installer to use the main version number since the build needs to include libraries and the DGS engine changes

--- installer/build.bat
+++ installer/build.bat
@@ -7,9 +7,6 @@ buildver.rb
 
 cd Installer
 
-
-buildver.rb
-
 c:\progra~1\NSIS\makensis /V2 DGSUtils.nsi
 
 SignCode -spc c:\keys\rts.spc -v c:\keys\rts.pvk -n "DGS Utilities Pack Setup" -i http://www.rtsz.com/products/dgs/ -$ commercial -t http://timestamp.verisign.com/scripts/timstamp.dll DGSUtils_Setup.exe
