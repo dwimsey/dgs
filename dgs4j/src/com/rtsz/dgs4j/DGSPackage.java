53a99ce5d21ba09c03822911b2f5df80be946270
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2008-08-29 23:08:42 +0000

    Missed adding this on last commit

---
+++ dgs4j/src/com/rtsz/dgs4j/DGSPackage.java
@@ -0,0 +1,176 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dgspreviewer;
+
+import ImageProcessor.*;
+
+import java.io.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+//import org.apache.xml.serialize.*;
+//import org.xml.sax.*;
+//import org.xml.sax.helpers.*;
+import org.w3c.dom.*;
+
+/**
+ *
+ * @author dwimsey
+ */
+public class DGSPackage {
+	String fileName = "";
+	String templateBuffer = "main";
+	float animationDuration = 0.0f;
+	float animationFramerate = 0.0f;
+
+	String commandString = "";
+
+	DGSFileInfo[] files = null;
+	DGSVariable[] variables = null;
+
+	public DGSPackage() {
+		super();
+		reset();
+	}
+
+	private void reset() {
+		fileName = "";
+		templateBuffer = "main";
+		animationDuration = 0.0f;
+		animationFramerate = 0.0f;
+		commandString = "";
+		files = null;
+		variables = null;
+	}
+
+	public boolean loadFile(String filename)
+	{
+		reset();
+		File file = null;
+		DocumentBuilderFactory dbf = null;
+		DocumentBuilder db = null;
+		Document doc = null;
+		try {
+			file = new File(filename);
+			dbf = DocumentBuilderFactory.newInstance();
+			db = dbf.newDocumentBuilder();
+		} catch (Exception ex) {
+//			setStatusMessage(10, "An unexpected error occurred creating the xml document for the DGS Package specified: " + filename + "  Error: " + ex.getLocalizedMessage());
+			ex.printStackTrace();
+			return (false);
+		}
+		try {
+			doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return (false);
+		}
+		try {
+			NodeList tLst = doc.getElementsByTagName("DGSCommands");
+			if(tLst.getLength()>0) {
+				NodeList nodeLst = tLst.item(0).getChildNodes();
+				Node dgsCommands = tLst.item(0);
+				this.commandString = dgsCommands.getTextContent();
+				
+				NamedNodeMap aMap = dgsCommands.getAttributes();
+				Node templateBufferNode = aMap.getNamedItem("templateBuffer");
+				Node animationDurationNode = aMap.getNamedItem("animationDuration");
+				Node animationFramerateNode = aMap.getNamedItem("animationFramerate");
+
+				if(templateBufferNode!=null) {
+					this.templateBuffer = templateBufferNode.getNodeValue();
+					if((this.templateBuffer == null) || (this.templateBuffer.length() == 0)) {
+						this.templateBuffer = "main"; // assume the fallback
+					}
+				}
+
+				String tStr;
+				if(animationDurationNode != null) {
+					tStr = animationDurationNode.getNodeValue();
+					if((tStr!=null) && (tStr.length()>0)) {
+						try {
+							this.animationDuration = Float.parseFloat(tStr);
+							if(this.animationDuration<Float.MIN_VALUE) {
+								this.animationDuration = 0.0f;
+							}
+						} catch (NumberFormatException ex) {
+							ex.printStackTrace();
+							return(false);
+						}
+					}
+				}
+				if(animationFramerateNode != null) {
+					tStr = animationFramerateNode.getNodeValue();
+					if((tStr!=null) && (tStr.length()>0)) {
+						try {
+							this.animationFramerate = Float.parseFloat(tStr);
+							if(this.animationFramerate<Float.MIN_VALUE) {
+								this.animationFramerate = 0.0f;
+							}
+						} catch (NumberFormatException ex) {
+							ex.printStackTrace();
+							return(false);
+						}
+					}
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return (false);
+		}
+
+	// this try wraps the variables reading
+		try {
+			NodeList nodeLst = doc.getElementsByTagName("DGSVariable");
+
+			int nLen = nodeLst.getLength();
+			DGSVariable vars[] = new DGSVariable[nLen];
+			for (int s = 0; s < nLen; s++) {
+				Node fstNode = nodeLst.item(s);
+				if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+					NamedNodeMap aMap = fstNode.getAttributes();
+					vars[s] = new DGSVariable(aMap.getNamedItem("name").getNodeValue(), aMap.getNamedItem("value").getNodeValue());
+				}
+			}
+			this.variables = vars;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return(false);
+		}
+
+		try {
+			NodeList nodeLst = doc.getElementsByTagName("DGSImageVariable");
+
+			int nLen = nodeLst.getLength();
+			DGSFileInfo vars[] = new DGSFileInfo[nLen];
+			for (int s = 0; s < nLen; s++) {
+				Node fstNode = nodeLst.item(s);
+				if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+					NamedNodeMap aMap = fstNode.getAttributes();
+					vars[s] = new DGSFileInfo();
+					vars[s].name = aMap.getNamedItem("name").getNodeValue();
+					vars[s].data = ImageProcessor.ProcessingEngine.Base64.decode(aMap.getNamedItem("data").getNodeValue());
+					if("jpg".equalsIgnoreCase(aMap.getNamedItem("mimeType").getNodeValue())) {
+							vars[s].mimeType = "image/jpeg";
+					} else {
+							vars[s].mimeType = "image/" + aMap.getNamedItem("mimeType").getNodeValue();
+					}
+					vars[s].width = Integer.valueOf(aMap.getNamedItem("width").getNodeValue());
+					vars[s].height = Integer.valueOf(aMap.getNamedItem("height").getNodeValue());
+				}
+			}
+			this.files = vars;
+		} catch (Exception ex) {
+//			setStatusMessage(10, "loadImageFiles: An error occurred parsing the variable data in" + varFileName + "\": " + ex.getLocalizedMessage());
+			ex.printStackTrace();
+			return(false);
+		}
+		this.fileName = filename;
+		return(true);
+	}
+}
