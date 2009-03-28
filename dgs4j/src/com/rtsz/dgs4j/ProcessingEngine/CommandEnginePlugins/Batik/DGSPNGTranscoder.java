0bb2fa28b9c0b71032a5c83396274429947f639c
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2009-03-28 18:47:00 +0000

    Scripting is now allowed

--- dgs4j/src/com/rtsz/dgs4j/ProcessingEngine/CommandEnginePlugins/Batik/DGSPNGTranscoder.java
+++ dgs4j/src/com/rtsz/dgs4j/ProcessingEngine/CommandEnginePlugins/Batik/DGSPNGTranscoder.java
@@ -12,8 +12,6 @@ public class DGSPNGTranscoder extends org.apache.batik.transcoder.image.PNGTrans
 
 	public DGSPNGTranscoder(ImageProcessor.ProcessingEngine.ProcessingWorkspace workspace) {
 		super();
-		this.hints.put(this.KEY_EXECUTE_ONLOAD, true);
-		this.hints.put(this.KEY_ALLOWED_SCRIPT_TYPES, ""); // "text/ecmascript");
 		this.userAgent = new DGSUserAgent(this.getUserAgent(), workspace);
 	}
 }
