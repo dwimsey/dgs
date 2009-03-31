c6715f4d45bba3af896822fc2bd41190d18b8bce
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2009-03-31 16:56:47 +0000

    Updated to use DGSPreviewCanvas for rendering of SVGs so the component can be easily shared without reimplementing all the headaches

--- trunk/DGSPreviewer/src/dgspreviewer/DGSPreviewerPanel.java
+++ trunk/DGSPreviewer/src/dgspreviewer/DGSPreviewerPanel.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dgspreviewer;
 
 import java.awt.image.BufferedImage;
@@ -12,25 +11,21 @@ import java.awt.image.BufferedImage;
  * @author dwimsey
  */
 public class DGSPreviewerPanel extends javax.swing.JPanel {
-    public BufferedImage image = null;
-    public dgspreviewer.Options options = null;
 
-    public void paint(java.awt.Graphics g)
-    {
-        if(options==null) {
-            super.paint(g);
-            return;
-        }
-        java.awt.Rectangle r = g.getClipBounds();
-        if(options.getBackgroundColor() == null) {
-            g.clearRect(r.x, r.y, r.width, r.height);
-        } else {
-            g.setColor(options.getBackgroundColor());
-            g.fillRect(r.x, r.y, r.width, r.height);
-        }
-        
-        if(image != null) {
-            g.drawImage(image, 0, 0, null);
-        }
-    }
+	public BufferedImage image = null;
+	public java.awt.Color BackgroundColor = new java.awt.Color(0x0000FFFF);
+
+	public void paint(java.awt.Graphics g) {
+		java.awt.Rectangle r = g.getClipBounds();
+		if (BackgroundColor == null) {
+			g.clearRect(r.x, r.y, r.width, r.height);
+		} else {
+			g.setColor(BackgroundColor);
+			g.fillRect(r.x, r.y, r.width, r.height);
+		}
+
+		if (image != null) {
+			g.drawImage(image, 0, 0, null);
+		}
+	}
 }
