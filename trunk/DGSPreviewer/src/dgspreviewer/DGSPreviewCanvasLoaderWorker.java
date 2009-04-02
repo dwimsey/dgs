d0cf5d2c4af29f428fd37dff30d88bd181142bd1
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2009-04-02 05:43:43 +0000

    Commented  out watermarking as it breaks on many images

--- trunk/DGSPreviewer/src/dgspreviewer/DGSPreviewCanvasLoaderWorker.java
+++ trunk/DGSPreviewer/src/dgspreviewer/DGSPreviewCanvasLoaderWorker.java
@@ -131,7 +131,7 @@ public class DGSPreviewCanvasLoaderWorker extends SwingWorker<DGSResponseInfo, V
 		} else {
 			dgsRequestInfo.instructionsXML += "<substituteVariables buffer=\"main\" />";
 		}
-		dgsRequestInfo.instructionsXML += "<addWatermark buffer=\"" + dPkg.templateBuffer + "\" srcImage=\"watermark\" opacity=\"0.05\"/>";
+//		dgsRequestInfo.instructionsXML += "<addWatermark buffer=\"" + dPkg.templateBuffer + "\" srcImage=\"watermark\" opacity=\"0.05\"/>";
 		dgsRequestInfo.instructionsXML += "<save ";
 		if((dPkg.animationDuration>0.0f) && (dPkg.animationFramerate>0.0f)) {
 //			dgsRequestInfo.instructionsXML += "animationDuration=\"" + dPkg.animationDuration + "\" animationFramerate=\"" + dPkg.animationFramerate + "\" ";
