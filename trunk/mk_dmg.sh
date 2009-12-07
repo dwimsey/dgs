cd7fb1f9a916ab9b6fcb162e53b0275bcbbe26dc
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2009-12-07 05:38:32 +0000

    Fixed path to dmg source directory

--- trunk/mk_dmg.sh
+++ trunk/mk_dmg.sh
@@ -4,7 +4,7 @@
 #
 #volume_name="$1"
 #disk_image_name="$2"
-source_directory="dmg_src"
+source_directory="dist/dmg_src"
 
 rm "$2" 2>/dev/null
 hdiutil create -fs HFS+ -srcfolder "$source_directory" -volname "$1" -format UDZO -imagekey zlig-level=9 -o "$2"
