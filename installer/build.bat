2e84bf532f0b6c0ffe4ab0069c8f4529bcc6deab
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2010-05-22 09:37:18 +0000

    Fixed installer build script to work with buildbot master build script

--- installer/build.bat
+++ installer/build.bat
@@ -1,12 +1,5 @@
 @echo off
 
 del DGSUtils_Setup.exe
-cd ..
-
-buildver.py
-
-cd Installer
 
 c:\progra~1\NSIS\makensis /V2 DGSUtils.nsi
-
-SignCode -spc c:\keys\rts.spc -v c:\keys\rts.pvk -n "DGS Utilities Pack Setup" -i http://www.rtsz.com/products/dgs/ -$ commercial -t http://timestamp.verisign.com/scripts/timstamp.dll DGSUtils_Setup.exe
