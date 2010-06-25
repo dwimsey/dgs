2f743aa9522b1dbbfea2f09258dd229e29520d19
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2010-06-25 08:53:31 +0000

    Minor path changes for current file locations

--- pkg_mac_files.bat
+++ pkg_mac_files.bat
@@ -20,12 +20,16 @@ copy ..\..\..\..\..\..\..\dgs4j\dist\dgs4j.jar .
 copy ..\..\..\..\..\..\..\libs\batik\batik-1.8pre\lib\batik-all.jar .
 copy ..\..\..\..\..\..\..\libs\batik\lib\*.jar .
 REM Delete the javascript jar file to ensure it doesn't run in the previewer at this time
-del js.jar
+REM del js.jar
 
 copy ..\..\..\..\..\..\..\libs\gif4free\dist\*.jar .
 
 
-cd ..\..\..\..\..\..\..
+cd ..\..\..\..\..
+mkdir .background
+cd .background
+copy ..\..\..\branding\dmgbackground.png" .
+cd ..\..\..
 
 del DGSPreviewerMacOSX.zip
 zip -r DGSPreviewerMacOSX.zip mac
