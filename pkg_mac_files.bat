b23aee6e7e6b1ea24c0505b406b544939772a0a7
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2010-06-25 05:12:03 +0000

    Initial building of files required for Mac packaging.

---
+++ pkg_mac_files.bat
@@ -0,0 +1,28 @@
+@echo off
+
+rmdir /s/q mac
+mkdir mac
+mkdir mac\DGSPreviewer
+mkdir mac\DGSPreviewer\DGSPreviewer.app
+
+xcopy /E /F DGSPreviewer\Bundle.app\*.* mac\DGSPreviewer\DGSPreviewer.app\
+
+cd mac\DGSPreviewer\DGSPreviewer.app\Contents\Resources\Java
+
+copy ..\..\..\..\..\..\DGSPreviewer\dist\DGSPreviewer.jar .
+
+echo Copying DGS dependancies ...
+mkdir lib
+cd lib
+
+copy ..\..\..\..\..\..\..\dgs4j\dist\dgs4j.jar .
+
+copy ..\..\..\..\..\..\..\libs\batik\batik-1.8pre\lib\batik-all.jar .
+copy ..\..\..\..\..\..\..\libs\batik\lib\*.jar .
+REM Delete the javascript jar file to ensure it doesn't run in the previewer at this time
+del js.jar
+
+copy ..\..\..\..\..\..\..\libs\gif4free\dist\*.jar .
+
+
+cd ..\..\..\..\..\..\..
