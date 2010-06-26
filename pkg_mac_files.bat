74738378b3f2d8767b72f36669812a1f6e0cfb6d
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2010-06-26 05:56:10 +0000

    Added call to dgs4cl to build the dmg background image from the template

--- pkg_mac_files.bat
+++ pkg_mac_files.bat
@@ -2,6 +2,29 @@
 
 rmdir /s/q mac
 mkdir mac
+
+mkdir mac\dgs4cl
+cd mac\dgs4cl
+
+xcopy ..\..\dgs4cl\dist\dgs4cl.jar .
+
+echo Copying dgs4cl dependancies ...
+mkdir lib
+cd lib
+
+xcopy ..\..\..\dgs4j\dist\dgs4j.jar .
+
+xcopy ..\..\..\libs\batik\batik-1.8pre\lib\batik-all.jar .
+xcopy ..\..\..\libs\batik\lib\*.jar .
+REM Delete the javascript jar file to ensure it doesn't run in the previewer at this time
+REM del js.jar
+
+xcopy ..\..\..\libs\gif4free\dist\*.jar .
+
+cd ..\..\..
+
+
+
 mkdir mac\DGSPreviewer
 mkdir mac\DGSPreviewer\DGSPreviewer.app
 
@@ -30,8 +53,7 @@ xcopy "C:\Program Files\NetBeans 6.8\java3\modules\ext\swing-worker-1.1.jar" .
 cd ..\..\..\..\..
 mkdir .background
 cd .background
-xcopy ..\..\..\branding\dmgbackground.png .\
-move dmgbackground.png background.png
+java -jar ..\..\..\mac\dgs4cl\dgs4cl.jar -d ..\..\branding\DGSPreviewerDMGBackground.xml ..\..\branding\DGSPreviewerDMGBackground.svg background.png
 cd ..\..\..
 
 del DGSPreviewerMacOSX.zip
