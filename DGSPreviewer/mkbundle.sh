b5dda5c815d18e9fcb1b2404494d00c5a2d569fc
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2011-05-10 05:19:28 +0000

    Added executable flag for script

--- DGSPreviewer/mkbundle.sh
+++ DGSPreviewer/mkbundle.sh
@@ -4,6 +4,7 @@ mkdir dist 2>/dev/null
 rm -rf dist/Bundle.app 2>/dev/null
 rm -rf dist/DGS\ Previewer.app 2>/dev/null
 cp -Rp Bundle.app dist
+mkdir dist/Bundle.app/Contents/Resources/Java
 cp dist/DGSPreviewer.jar dist/Bundle.app/Contents/Resources/Java
 cp dist/lib/*.jar dist/Bundle.app/Contents/Resources/Java
 
