1b8fbaec8337dec9585e53fac0d7e642beb16be1
david@wimsey.us <david@wimsey.us@aa8f7e95-1b12-3e66-f59f-aa753503de59>
2012-12-09 23:00:06 +0000

    Updated DMG creation bits

--- DGSPreviewer/mkbundle.sh
+++ DGSPreviewer/mkbundle.sh
@@ -6,7 +6,8 @@ rm -rf dist/DGS\ Previewer.app 2>/dev/null
 cp -Rp Bundle.app dist
 mkdir dist/Bundle.app/Contents/Resources/Java
 cp dist/DGSPreviewer.jar dist/Bundle.app/Contents/Resources/Java
-cp dist/lib/*.jar dist/Bundle.app/Contents/Resources/Java
+mkdir dist/Bundle.app/Contents/Resources/Java/lib
+cp dist/lib/*.jar dist/Bundle.app/Contents/Resources/Java/lib/
 
-/Developer/Tools/SetFile -a B dist/Bundle.app
+SetFile -a B dist/Bundle.app
 mv dist/Bundle.app "dist/DGS Previewer.app"
